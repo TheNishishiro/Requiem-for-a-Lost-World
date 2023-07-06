9e7186655585657bce9ef1a41d1db2de2bcc5904
Piotr Kruszewski <32763677+TheNishishiro@users.noreply.github.com>
2023-07-06 21:34:28 +0200

    Add DiscordGameSDK for game management.
    This large commit introduces the DiscordGameSDK to the codebase to improve integration with Discord. It includes classes, methods, and comprehensive structures that the SDK provides for improving social features. This allows interaction with Discord in various ways, such as updating activities, managing storage and stores, joining and creating lobbies, etc. The SDK can help with managing Discord related features in a unified way.
    Also added metadata files associated with the new files for Unity’s internal use.

---
+++ Assets/Scripts Shared/Managers/DiscordManager.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Discord;
+using UnityEngine;
+using Discord = Discord.Discord;
+
+namespace Managers
+{
+	public class DiscordManager : MonoBehaviour
+	{
+		private global::Discord.Discord _discord;
+		private ActivityManager _activityManager;
+		
+		public void Awake()
+		{
+			var instances = FindObjectsOfType<AchievementManager>();
+			if (instances.Length > 1)
+			{
+				Destroy(gameObject);
+				return;
+			}
+			
+			_discord = new global::Discord.Discord(1126131759722012674, (ulong)CreateFlags.NoRequireDiscord);
+			_activityManager = _discord.GetActivityManager();
+			DontDestroyOnLoad(gameObject);
+		}
+
+		private void Update()
+		{
+			_discord.RunCallbacks();
+		}
+
+		public void UpdateActivity(string details, string state, string image, string imageTitle)
+		{
+			return;
+			
+			var activity = new global::Discord.Activity()
+			{
+				State = state,
+				Details = details,
+				Assets =
+				{
+					LargeImage = "icon",
+					LargeText = "RogueLite",
+					SmallImage = image.ToLower(),
+					SmallText = imageTitle
+				},
+				Timestamps =
+				{
+					Start = DateTimeOffset.UtcNow.ToUnixTimeSeconds(),
+				},
+				Type = ActivityType.Playing
+			};
+
+			_activityManager.UpdateActivity(activity, (res) =>
+			{
+				if (res == global::Discord.Result.Ok)
+				{
+					Debug.Log("Discord activity updated!");
+				}
+				else
+				{
+					Debug.LogError($"Discord activity failed to update: {res}");
+				}
+			});
+		}
+	}
+}
