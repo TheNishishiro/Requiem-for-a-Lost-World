774bd15b830be223b1530661791135ddca23c47f
Piotr Kruszewski <32763677+TheNishishiro@users.noreply.github.com>
2023-07-14 17:39:43 +0200

    Update Main Menu UI and tweak enemy and player attributes
    This commit involves general improvements and tweaks. Firstly, we significantly revamped the Main Menu UI, adding some new elements and adjusting the render queue of shaders to better fit the new design. Two new GameObjects, Background and Dim, were also introduced for improved appearance and functionality.
    
    On the game play side, we slightly decreased the increase in item pickup range per level of the Magnet upgrade, tuning it down from 0.25m to 0.1m for better balance. Additionally, the enemy character "grand octi" from the "Peace Ender" series received an update, including modifying its position and scale in the Main Level scene, and tweaking its properties and attributes.
    
    A new file, "TextGlowPulse.cs", was created under "Assets/UI/Shared/Animations/" for handling text animation, enriching the visual effects of UI. Also, a new enemy character "Demon Lord" has been introduced.
    
    Overall, these updates aim at enhancing user experience and game balance.

---
+++ Assets/Objects/Enemies/Peace Ender/PEAttackComponent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace Objects.Enemies.Peace_Ender
+{
+	public class PEAttackComponent : MonoBehaviour
+	{
+		private ChaseComponent _chaseComponent;
+
+		private void Awake()
+		{
+			_chaseComponent = GetComponentInParent<ChaseComponent>();
+		}
+	}
+}
---
+++ Assets/UI/Shared/Animations/TextGlowPulse.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+using TMPro;
+
+namespace UI.Shared.Animations
+{
+    public class TextGlowPulse : MonoBehaviour
+    {
+        public float PulseSpeed = 1f;
+        public float EmissionIntensityMin = 0f;
+        public float EmissionIntensityMax = 1f;
+      
+        private TextMeshProUGUI _tmpText;
+
+        private void Awake()
+        {
+            _tmpText = GetComponent<TextMeshProUGUI>();
+        }
+
+        private void Update() 
+        {
+          float glow = EmissionIntensityMin + ((EmissionIntensityMax - EmissionIntensityMin) * (Mathf.Sin(Time.time * PulseSpeed) + 1f) / 2f);
+          _tmpText.fontMaterial.SetFloat("_GlowPower", glow);
+        }
+    }
+}
