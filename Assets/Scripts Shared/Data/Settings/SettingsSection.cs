398ca46228e932e35905f254c117c3e5669f7cbf
nishi <nishishirokun@gmail.com>
2024-04-14 18:10:56 +0200

    feat(UI): rework settings layout, add settings presets

---
+++ Assets/Scripts Shared/Data/Settings/SettingsSection.cs
@@ -0,0 +1,10 @@
+﻿namespace DefaultNamespace.Data.Settings
+{
+    public enum SettingsSection
+    {
+        Graphics = 0,
+        Sound = 1,
+        Integration = 2,
+        Multiplayer = 3
+    }
+}
--- Assets/UI/Main Menu/REWORK/Scripts/Managers/SettingsScreenManager.cs
+++ Assets/UI/Main Menu/REWORK/Scripts/Managers/SettingsScreenManager.cs
@@ -2,25 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using DefaultNamespace.Data;
+using DefaultNamespace.Data.Settings;
 using Interfaces;
 using Managers;
 using NaughtyAttributes;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace UI.Main_Menu.REWORK.Scripts
 {
     public class SettingsScreenManager : MonoBehaviour, IStackableWindow
     {
-        [BoxGroup("UI Panels")] [SerializeField] private GameObject panelSection;
-        [BoxGroup("UI Panels")] [SerializeField] private GameObject panelSubSection;
-        [BoxGroup("UI Panels")] [SerializeField] private GameObject panelSettings;
+        [BoxGroup("Buttons")] [SerializeField] private List<Button> buttonSections;
         [Space]
-        [BoxGroup("Settings Containers")] [SerializeField] private GameObject containerScreenSettings;
+        [BoxGroup("Styling")] [SerializeField] private Material materialSelectedText;
+        [BoxGroup("Styling")] [SerializeField] private Material materialIdleText;
+        [BoxGroup("Styling")] [SerializeField] private Color colorHighlight;
         [Space]
-        [BoxGroup("Labels")] [SerializeField] private TextMeshProUGUI labelSubSectionPanelTitle;
-        [BoxGroup("Labels")] [SerializeField] private TextMeshProUGUI labelSubSectionContainerTitle;
+        [BoxGroup("Section Containers")] [SerializeField] private GameObject containerGraphicSettings;
+        [BoxGroup("Section Containers")] [SerializeField] private GameObject containerSoundSettings;
+        [BoxGroup("Section Containers")] [SerializeField] private GameObject containerIntegrationSettings;
+        [BoxGroup("Section Containers")] [SerializeField] private GameObject containerMultiplayerSettings;
         [Space]
+        [BoxGroup("Settings Entries")] [SerializeField] private SettingsEntry entryPreset;
         [BoxGroup("Settings Entries")] [SerializeField] private SettingsEntry entryResolution;
         [BoxGroup("Settings Entries")] [SerializeField] private SettingsEntry entryWindowMode;
         [BoxGroup("Settings Entries")] [SerializeField] private SettingsEntry entryVSync;
@@ -38,18 +43,14 @@ namespace UI.Main_Menu.REWORK.Scripts
         [BoxGroup("Settings Entries")] [SerializeField] private SettingsEntry entryCoopNickname;
         [BoxGroup("Settings Entries")] [SerializeField] private SettingsEntry entryCoopDisplayProjectiles;
         [BoxGroup("Settings Entries")] [SerializeField] private SettingsEntry entryVolume;
-        [Space]
-        [BoxGroup("Entries")] [SerializeField] private List<SettingSection> mainSettingSections;
-        [Space]
-        [BoxGroup("Animations")] [SerializeField] private Animator animator;
         
-        private List<Resolution> availableResolutions = new ();
-        private const int minResolutionWidth = 800;
-        private const int minResolutionHeight = 600;
+        private readonly List<Resolution> _availableResolutions = new ();
+        private const int MinResolutionWidth = 800;
+        private const int MinResolutionHeight = 600;
         
         private SaveFile _saveFile;
-        private int _selectionIndex;
-        private const float KeyHoldDelay = 0.2f;
+        private int _currentSectionId;
+        private const float KeyHoldDelay = 0.25f;
         private float _keyNextActionTime = 0f;
         private bool _isSubsectionOpened;
         
@@ -60,46 +61,46 @@ namespace UI.Main_Menu.REWORK.Scripts
             if (Input.GetKeyDown(KeyCode.Escape))
                 Close();
             
-            if (Input.GetKeyDown(KeyCode.Return))
-                mainSettingSections[_selectionIndex].Activate();
-            
             if (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.DownArrow)) 
                 _keyNextActionTime = 0;
             
             if (Input.GetKey(KeyCode.UpArrow) && Time.time > _keyNextActionTime)
             {
                 _keyNextActionTime = Time.time + KeyHoldDelay;
-                _selectionIndex--;
-                if (_selectionIndex < 0) _selectionIndex = mainSettingSections.Count - 1;
-                UpdateSelection(mainSettingSections[_selectionIndex].GetInstanceID());
+                _currentSectionId--;
+                if (_currentSectionId < 0) _currentSectionId = buttonSections.Count - 1;
+                FilterSection(_currentSectionId);
             }
             else if (Input.GetKey(KeyCode.DownArrow) && Time.time > _keyNextActionTime)
             {
                 _keyNextActionTime = Time.time + KeyHoldDelay;
-                _selectionIndex++;
-                if (_selectionIndex >= mainSettingSections.Count) _selectionIndex = 0;
-                UpdateSelection(mainSettingSections[_selectionIndex].GetInstanceID());
-            }    
+                _currentSectionId++;
+                if (_currentSectionId >= buttonSections.Count) _currentSectionId = 0;
+                FilterSection(_currentSectionId);
+            }   
         }
 
-        public void UpdateSelection(int instanceId)
+        public void FilterSection(int sectionId)
         {
-            mainSettingSections.ForEach(x => x.SetActive(false));
-            _selectionIndex = mainSettingSections.FindIndex(x => x.GetInstanceID() == instanceId);
-            mainSettingSections[_selectionIndex].SetActive(true);
+            _currentSectionId = sectionId;
+            var section = (SettingsSection)sectionId;
+            containerGraphicSettings.SetActive(section is SettingsSection.Graphics);
+            containerSoundSettings.SetActive(section is SettingsSection.Sound);
+            containerIntegrationSettings.SetActive(section is SettingsSection.Integration);
+            containerMultiplayerSettings.SetActive(section is SettingsSection.Multiplayer);
+            
+            buttonSections.ForEach(x => x.GetComponent<Image>().color = Color.clear);
+            buttonSections.ForEach(x => x.GetComponentInChildren<TextMeshProUGUI>().fontSharedMaterial = materialIdleText);
+            buttonSections[sectionId].GetComponent<Image>().color = colorHighlight;
+            buttonSections[sectionId].GetComponentInChildren<TextMeshProUGUI>().fontSharedMaterial = materialSelectedText;
         }
-
+        
+        
         public void Open()
         {
             _saveFile = SaveManager.instance.GetSaveFile();
-            panelSubSection.SetActive(false);
-            panelSettings.SetActive(false);
-            foreach (var settingsContainer in GetComponentsInChildren<SettingsContainer>())
-                settingsContainer.gameObject.SetActive(false);
-            foreach (var subSectionContainer in GetComponentsInChildren<SettingsSubSectionContainer>())
-                subSectionContainer.gameObject.SetActive(false);
-            
             LoadSettings();
+            FilterSection(0);
             StackableWindowManager.instance.OpenWindow(this);
         }
 
@@ -109,17 +110,102 @@ namespace UI.Main_Menu.REWORK.Scripts
             StackableWindowManager.instance.CloseWindow(this);
         }
 
+        public void ApplyPreset()
+        {
+            switch (entryPreset.GetSelectedOption())
+			{
+				case 0:
+					entryVSync.SetSelection(0);
+					entryGrassRenderDistance.SetSelection(0);
+					entryGrassDensity.SetSelection(0);
+					entryVfxQuality.SetSelection(0);
+					entryTextureQuality.SetSelection(0);
+					entryRenderScaling.SetSelection(2);
+					entryShadows.SetSelection(0);
+					entryAntialiasing.SetSelection(0);
+					entryLod.SetSelection(0);
+					entryRenderDistance.SetSelection(0);
+					entry3dGrass.SetSelection(0);
+					break;
+				case 1:
+					entryVSync.SetSelection(0);
+					entryGrassRenderDistance.SetSelection(1);
+					entryGrassDensity.SetSelection(1);
+					entryVfxQuality.SetSelection(1);
+					entryTextureQuality.SetSelection(2);
+					entryRenderScaling.SetSelection(2);
+					entryShadows.SetSelection(0);
+					entryAntialiasing.SetSelection(0);
+					entryLod.SetSelection(1);
+					entryRenderDistance.SetSelection(0);
+					entry3dGrass.SetSelection(0);
+					break;
+				case 2:
+					entryVSync.SetSelection(1);
+					entryGrassRenderDistance.SetSelection(2);
+					entryGrassDensity.SetSelection(2);
+					entryVfxQuality.SetSelection(2);
+					entryTextureQuality.SetSelection(4);
+					entryRenderScaling.SetSelection(3);
+					entryShadows.SetSelection(1);
+					entryAntialiasing.SetSelection(1);
+					entryLod.SetSelection(1);
+					entryRenderDistance.SetSelection(1);
+					entry3dGrass.SetSelection(0);
+					break;
+				case 3:
+					entryVSync.SetSelection(1);
+					entryGrassRenderDistance.SetSelection(2);
+					entryGrassDensity.SetSelection(3);
+					entryVfxQuality.SetSelection(3);
+					entryTextureQuality.SetSelection(4);
+					entryRenderScaling.SetSelection(4);
+					entryShadows.SetSelection(3);
+					entryAntialiasing.SetSelection(2);
+					entryLod.SetSelection(3);
+					entryRenderDistance.SetSelection(1);
+					entry3dGrass.SetSelection(0);
+					break;
+				case 4:
+					entryVSync.SetSelection(1);
+					entryGrassRenderDistance.SetSelection(3);
+					entryGrassDensity.SetSelection(3);
+					entryVfxQuality.SetSelection(4);
+					entryTextureQuality.SetSelection(4);
+					entryRenderScaling.SetSelection(4);
+					entryShadows.SetSelection(4);
+					entryAntialiasing.SetSelection(3);
+					entryLod.SetSelection(3);
+					entryRenderDistance.SetSelection(2);
+					entry3dGrass.SetSelection(1);
+					break;
+				case 5:
+					entryVSync.SetSelection(1);
+					entryGrassRenderDistance.SetSelection(4);
+					entryGrassDensity.SetSelection(3);
+					entryVfxQuality.SetSelection(4);
+					entryTextureQuality.SetSelection(4);
+					entryRenderScaling.SetSelection(4);
+					entryShadows.SetSelection(5);
+					entryAntialiasing.SetSelection(3);
+					entryLod.SetSelection(4);
+					entryRenderDistance.SetSelection(3);
+					entry3dGrass.SetSelection(1);
+					break;
+			}
+        }
+
         private void LoadSettings()
         {
             var configuration = _saveFile.ConfigurationFile;
             foreach (var resolution in Screen.resolutions)
             {
-                if (resolution.width < minResolutionWidth || resolution.height < minResolutionHeight) 
+                if (resolution.width < MinResolutionWidth || resolution.height < MinResolutionHeight) 
                     continue;
 				
-                availableResolutions.Add(resolution);
+                _availableResolutions.Add(resolution);
             }
-            entryResolution.SetOptions(availableResolutions.Select(x => $"{x.width}x{x.height} @{x.refreshRateRatio.value:0}hz").ToArray(), GetResolutionIndex(configuration.ResolutionWidth, configuration.ResolutionHeight, configuration.RefreshRate));
+            entryResolution.SetOptions(_availableResolutions.Select(x => $"{x.width}x{x.height} @{x.refreshRateRatio.value:0}hz").ToArray(), GetResolutionIndex(configuration.ResolutionWidth, configuration.ResolutionHeight, configuration.RefreshRate));
             entryWindowMode.SetSelection(configuration.WindowMode);
             entryVSync.SetSelection(configuration.Vsync ? 1 : 0);
             entryRenderScaling.SetSelection(configuration.RenderScaling);
@@ -142,9 +228,9 @@ namespace UI.Main_Menu.REWORK.Scripts
         {
             var configuration = _saveFile.ConfigurationFile;
 
-            configuration.ResolutionWidth = availableResolutions[entryResolution.GetSelectedOption()].width;
-            configuration.ResolutionHeight = availableResolutions[entryResolution.GetSelectedOption()].height;
-            configuration.RefreshRate = availableResolutions[entryResolution.GetSelectedOption()].refreshRateRatio.numerator;
+            configuration.ResolutionWidth = _availableResolutions[entryResolution.GetSelectedOption()].width;
+            configuration.ResolutionHeight = _availableResolutions[entryResolution.GetSelectedOption()].height;
+            configuration.RefreshRate = _availableResolutions[entryResolution.GetSelectedOption()].refreshRateRatio.numerator;
             configuration.WindowMode = entryWindowMode.GetSelectedOption();
             configuration.Vsync = entryVSync.GetSelectedOption() == 1;
             configuration.RenderScaling = entryRenderScaling.GetSelectedOption();
@@ -168,13 +254,13 @@ namespace UI.Main_Menu.REWORK.Scripts
         
         private int GetResolutionIndex(int width, int height, uint refreshRate)
         {
-            var bestMatchResolution = availableResolutions[0];
+            var bestMatchResolution = _availableResolutions[0];
             var bestMatchIndex = -1;
             var hasMatchingResolution = false;
 
-            for (var i = 0; i < availableResolutions.Count; i++)
+            for (var i = 0; i < _availableResolutions.Count; i++)
             {
-                var resolution = availableResolutions[i];
+                var resolution = _availableResolutions[i];
                 if (resolution.width == width && resolution.height == height)
                 {
                     hasMatchingResolution = true;
@@ -200,44 +286,13 @@ namespace UI.Main_Menu.REWORK.Scripts
 
             // If we found a matching resolution with best available refresh rate, return that index
             // Otherwise return highest resolution available
-            return hasMatchingResolution ? bestMatchIndex : availableResolutions.Count - 1;   
+            return hasMatchingResolution ? bestMatchIndex : _availableResolutions.Count - 1;   
         }
         
         public bool IsInFocus { get; set; }
         public void SetActive(bool isActive)
         {
             gameObject.SetActive(isActive);
-        }
-
-        public void OpenSubSectionPanel()
-        {
-            _isSubsectionOpened = true;
-            panelSubSection.SetActive(true);
-        }
-
-        public void CloseSubSectionPanel()
-        {
-            var sectionComponent = panelSubSection.GetComponentInChildren<SettingsSubSectionContainer>();
-            if (sectionComponent != null)
-                sectionComponent.gameObject.SetActive(false);
-            if (panelSettings.activeSelf)
-                CloseSettingsPanel();
-                
-            _isSubsectionOpened = false;
-            panelSubSection.SetActive(false);
-        }
-
-        public void OpenSettingsPanel()
-        {
-            panelSettings.SetActive(true);
-        }
-
-        public void CloseSettingsPanel()
-        {
-            var settingsContainer = panelSettings.GetComponentInChildren<SettingsContainer>();
-            if (settingsContainer != null)
-                settingsContainer.Close();
-            panelSettings.SetActive(false);
         }
 
         public void LiveAdjustVolume()
