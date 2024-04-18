1f2cb6f51b859b7f11a59e237dd5dae4546426e1
nishi <nishishirokun@gmail.com>
2024-04-18 21:02:09 +0200

    Add UI Extensions

---
+++ Assets/Editor/MoveToTrailUVEditor.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(MoveToTrailUV))][CanEditMultipleObjects]
+public class MoveToTrailUVEditor : Editor
+{
+    SerializedProperty m_moveObject_sp;
+    SerializedProperty m_shaderPropertyName_sp;
+    SerializedProperty m_shaderPropertyID_sp;
+    SerializedProperty m_materialData_sp;
+
+    private MoveToTrailUV m_mttuv;
+
+    private void OnEnable()
+    {
+        // 기능 봉인 // m_overrideMaterial_sp = serializedObject.FindProperty("m_overrideMaterial");
+        m_moveObject_sp = serializedObject.FindProperty("m_moveObject");
+        m_shaderPropertyName_sp = serializedObject.FindProperty("m_shaderPropertyName");
+        m_shaderPropertyID_sp = serializedObject.FindProperty("m_shaderPropertyID");
+        m_materialData_sp = serializedObject.FindProperty("m_materialData");
+
+        m_mttuv = target as MoveToTrailUV;
+        
+        serializedObject.Update();
+        InitializeEditor();
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    private void OnDisable()
+    {
+        serializedObject.Update();
+        for (int i = 0; i < m_materialData_sp.arraySize; i++)
+        {
+            SerializedProperty materialDataElement_sp = m_materialData_sp.GetArrayElementAtIndex(i);
+            TrailRenderer trailRenderer = (TrailRenderer)materialDataElement_sp.FindPropertyRelative("m_trailRenderer").objectReferenceValue;
+            if (trailRenderer != null)
+            {
+                Material mat = trailRenderer.sharedMaterial;
+                if (mat != null)
+                {
+                    mat.SetFloat(m_shaderPropertyID_sp.intValue, 0f);
+                }
+            }
+        }
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        int checkTrailRenderertile = CheckTrailRendererTile();
+        if (checkTrailRenderertile != -1)
+        {
+            string message = String.Format("Element {0}번 Trail Renderer의 Texture Mode가 Tile이 아닙니다.", checkTrailRenderertile);
+            EditorGUILayout.HelpBox(message, MessageType.Warning);
+        }
+
+        string checkTrailRendererShader = CheckTrailRendererShader();
+        if (checkTrailRendererShader != "")
+        {
+            EditorGUILayout.HelpBox(checkTrailRendererShader, MessageType.Warning);
+        }
+
+        EditorGUI.BeginChangeCheck();
+        {
+            EditorGUILayout.PropertyField(m_moveObject_sp);
+            EditorGUILayout.PropertyField(m_shaderPropertyName_sp);
+            EditorGUILayout.PropertyField(m_materialData_sp);
+        }
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(target, "MoveToTrailUV changed");
+            InitializeEditor(); // 뭔가 변화가 생기면 초기화
+        }
+
+        for (int i = 0; i < m_materialData_sp.arraySize; i++)
+        {
+            SerializedProperty materialDataElement_sp = m_materialData_sp.GetArrayElementAtIndex(i);
+            SyncElementTiling(materialDataElement_sp);
+        }
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    private void SyncElementTiling(SerializedProperty materialDataElement_sp) 
+    {
+        TrailRenderer trailRenderer = (TrailRenderer)materialDataElement_sp.FindPropertyRelative("m_trailRenderer").objectReferenceValue;
+        if (trailRenderer != null)
+        {
+            Material mat = trailRenderer.sharedMaterial;
+            if (mat != null)
+            {
+                if (materialDataElement_sp.FindPropertyRelative("m_uvTiling").vector2Value != mat.mainTextureScale)
+                {
+                    materialDataElement_sp.FindPropertyRelative("m_uvTiling").vector2Value = mat.mainTextureScale;
+                }
+            }
+        }
+    }
+
+    private void InitializeEditor()
+    {
+        if (m_materialData_sp.serializedObject.targetObject == null || m_materialData_sp.arraySize == 0)
+            return;
+
+        m_shaderPropertyID_sp.intValue = Shader.PropertyToID(m_shaderPropertyName_sp.stringValue);
+
+        for (int i = 0; i < m_materialData_sp.arraySize; i++)
+        {
+            SerializedProperty materialDataElement_sp = m_materialData_sp.GetArrayElementAtIndex(i);
+            SyncElementTiling(materialDataElement_sp);
+
+            materialDataElement_sp.FindPropertyRelative("m_move").floatValue = 0f;
+        }
+    }
+
+    private int CheckTrailRendererTile()
+    {
+        if (m_mttuv.m_materialData.Length == 0)
+            return -1;
+
+        for (int i = 0; i < m_mttuv.m_materialData.Length; i++)
+        {
+            if (m_mttuv.m_materialData[i].m_trailRenderer == null)
+                continue;
+            TrailRenderer trailRenderer = (TrailRenderer)m_mttuv.m_materialData[i].m_trailRenderer;
+            if (trailRenderer.textureMode != LineTextureMode.Tile)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private string CheckTrailRendererShader()
+    {
+        if (m_mttuv.m_materialData.Length == 0)
+            return "";
+
+        for (int i = 0; i < m_mttuv.m_materialData.Length; i++)
+        {
+            TrailRenderer trailRenderer = m_mttuv.m_materialData[i].m_trailRenderer;
+            if (trailRenderer == null)
+                continue;
+            Material mat = trailRenderer.sharedMaterial;
+            if (mat == null)
+            {
+                string message = String.Format("Element {0}번째 렌더러의 재질이 없습니다.", i);
+                return message;
+            }
+        }
+
+        return "";
+    }
+}
---
+++ Assets/Scripts Shared/Utilities/MoveToTrailUV.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+
+[ExecuteAlways]
+public class MoveToTrailUV : MonoBehaviour
+{
+    [System.Serializable]
+    public struct MaterialData
+    {
+        public MaterialData(TrailRenderer trailRenderer, Material material, Vector2 uvScale, float move)
+        {
+            m_trailRenderer = trailRenderer;
+            m_uvTiling = uvScale;
+            m_move = move;
+        }
+        
+        public TrailRenderer m_trailRenderer;
+        [HideInInspector] public Vector2 m_uvTiling;
+        [HideInInspector] public float m_move;
+    }
+    
+    public Transform m_moveObject;
+    public string m_shaderPropertyName = "_MoveToMaterialUV";
+    public int m_shaderPropertyID;
+    public MaterialData[] m_materialData = new MaterialData[1] { new MaterialData ( null, null, new Vector2(1, 1), 0f ) };
+
+    private Vector3 m_beforePosW = Vector3.zero;
+    void Start()
+    {
+        Initialize();
+    }
+
+    void LateUpdate()
+    {
+        if (m_moveObject == null)
+            return;
+        if (m_materialData == null || m_materialData.Length == 0)
+            return;
+
+        Vector3 nowPosW = m_moveObject.transform.position;
+        if (nowPosW == m_beforePosW)
+            return; 
+        
+        float distance = Vector3.Distance(nowPosW, m_beforePosW);
+        m_beforePosW = nowPosW;
+
+        for (int i = 0; i < m_materialData.Length; i++)
+        {
+            if (m_materialData[i].m_trailRenderer == null)
+                continue;
+
+            m_materialData[i].m_move += distance * m_materialData[i].m_uvTiling.x;
+            if (m_materialData[i].m_move > 1f)
+            {
+                m_materialData[i].m_move = m_materialData[i].m_move % 1f;
+            }
+
+            TrailRenderer trailRenderer = m_materialData[i].m_trailRenderer;
+            if (trailRenderer != null)
+            {
+                Material mat = trailRenderer.sharedMaterial;
+                if (mat != null)
+                {
+                    mat.SetFloat(m_shaderPropertyID, m_materialData[i].m_move);
+                }
+            }
+        }
+    }
+
+    public void Initialize()
+    {
+        if (m_materialData == null || m_materialData.Length == 0)
+            return;
+        
+        m_shaderPropertyID = Shader.PropertyToID(m_shaderPropertyName);
+
+        for (int i = 0; i < m_materialData.Length; i++)
+        {
+            m_materialData[i].m_move = 0f;
+            TrailRenderer trailRenderer = m_materialData[i].m_trailRenderer;
+            if (trailRenderer != null)
+            {
+                Material mat = trailRenderer.sharedMaterial;
+                if (mat != null)
+                {
+                    m_materialData[i].m_uvTiling = mat.mainTextureScale;
+                }
+            }
+        }
+    }
+}
