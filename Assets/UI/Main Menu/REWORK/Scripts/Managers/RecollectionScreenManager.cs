9ba1ab200234b60caf29a8aa596d7f5bbc45b98c
nishi <nishishirokun@gmail.com>
2024-04-20 09:36:57 +0200

    feat(RecollectionScreenManager): remove weighted characters after selection

--- Assets/UI/Main Menu/REWORK/Scripts/Managers/RecollectionScreenManager.cs
+++ Assets/UI/Main Menu/REWORK/Scripts/Managers/RecollectionScreenManager.cs
@@ -221,11 +221,16 @@ namespace UI.Main_Menu.REWORK.Scripts
             }    
         
             saveFile.CurrentBannerCharacterId = weightedCharacters.GetNextRandom();
+            weightedCharacters.Remove(saveFile.CurrentBannerCharacterId);
             saveFile.GetCharacterSaveData(saveFile.CurrentBannerCharacterId).LastBannerDate = DateTime.Now;
             
+            
             saveFile.CurrentBannerSubCharacterId1 = weightedCharacters.GetNextRandom();
+            weightedCharacters.Remove(saveFile.CurrentBannerSubCharacterId1);
             saveFile.CurrentBannerSubCharacterId2 = weightedCharacters.GetNextRandom();
+            weightedCharacters.Remove(saveFile.CurrentBannerSubCharacterId2);
             saveFile.CurrentBannerSubCharacterId3 = weightedCharacters.GetNextRandom();
+            weightedCharacters.Remove(saveFile.CurrentBannerSubCharacterId3);
         
             var now = DateTime.Now;
             var diffInDays = 0;
