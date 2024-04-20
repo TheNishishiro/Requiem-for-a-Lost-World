5d0d7dba9ad4fa9eac1d9081910da42c8a6702f7
nishi <nishishirokun@gmail.com>
2024-04-20 09:54:09 +0200

    feat(UI): extend StoryReader functionality to provide a typewriter text effect and the ability to skip reading.

--- Assets/UI/Main Menu/REWORK/Scripts/StoryReader.cs
+++ Assets/UI/Main Menu/REWORK/Scripts/StoryReader.cs
@@ -1,4 +1,7 @@
-﻿using TMPro;
+﻿using System;
+using System.Collections;
+using System.Text;
+using TMPro;
 using UnityEngine;
 
 namespace UI.Main_Menu.REWORK.Scripts
@@ -6,10 +9,52 @@ namespace UI.Main_Menu.REWORK.Scripts
     public class StoryReader : MonoBehaviour
     {
         [SerializeField] private TextMeshProUGUI textContent;
-
+        private string _currentText;
+        private bool _skipRequest;
+        private const float TextSpeed = 0.05f;
+        private Coroutine _readingRoutine;
+        private readonly StringBuilder _stringBuilder = new();
+        
         public void SetText(TextAsset textFile)
         {
-            textContent.text = textFile.text;
+            _currentText = textFile.text;
+            _skipRequest = false;
+        }
+
+        public void OnEnable()
+        {
+            if (_readingRoutine != null)
+            {
+                StopCoroutine(_readingRoutine);
+            }
+            _readingRoutine = StartCoroutine(ReadText());
+        }
+
+
+        private IEnumerator ReadText()
+        {
+            _stringBuilder.Clear();
+
+            foreach (var character in _currentText.ToCharArray())
+            {
+                if (_skipRequest)
+                {
+                    _stringBuilder.Clear();
+                    _stringBuilder.Append(_currentText);
+                    break;
+                }
+
+                _stringBuilder.Append(character);
+                textContent.text = _stringBuilder.ToString();
+                yield return new WaitForSeconds(TextSpeed);
+            }
+
+            _skipRequest = false;
+        }
+        
+        public void SkipReading()
+        {
+            _skipRequest = true;
         }
     }
 }
