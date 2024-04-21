0c25882bc7ba9caadcf305f9b1fbc900e85660b1
nishi <nishishirokun@gmail.com>
2024-04-21 22:46:57 +0200

    feat(Environment): Add drop on destroy condition to Destructable object

--- Assets/Objects/Environment/Destructable.cs
+++ Assets/Objects/Environment/Destructable.cs
@@ -7,18 +7,22 @@ namespace Objects.Environment
 	public class Destructable : MonoBehaviour
 	{
 		private Damageable damageable;
+		[SerializeField] private DropOnDestroy dropOnDestroy;
+		private bool _isDropping;
 		
 		public void Awake()
 		{
 			damageable = GetComponent<Damageable>();
 			damageable.SetHealth(1);
+			_isDropping = false;
 		}
 
 		public void Update()
 		{
-			if (damageable.IsDestroyed())
+			if (damageable.IsDestroyed() && !_isDropping)
 			{
-				GetComponent<DropOnDestroy>()?.CheckDrop();
+				_isDropping = true;
+				dropOnDestroy.CheckDrop();
 				Destroy(gameObject);
 			}
 		}
