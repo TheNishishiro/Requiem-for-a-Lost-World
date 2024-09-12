7e05ac63257055e3bcedb64237ef6ba2ef01321c
nishi <nishishirokun@gmail.com>
2024-09-12 21:41:08 +0200

    fix typo

--- Assets/Objects/Characters/CharacterRank.cs
+++ Assets/Objects/Characters/CharacterRank.cs
@@ -4,17 +4,17 @@ namespace Objects.Characters
 {
 	public enum CharacterRank
 	{
-		[StringValue("S")]
+		[StringValue("\u2727\u2727\u2727\u2727\u2727")]
 		E0 = 0,
-		[StringValue("S<sub>I</sub>")]
+		[StringValue("\u2726\u2727\u2727\u2727\u2727")]
 		E1 = 1,
-		[StringValue("SS")]
+		[StringValue("\u2726\u2726\u2727\u2727\u2727")]
 		E2 = 2,
-		[StringValue("SS<sub>I</sub>")]
+		[StringValue("\u2726\u2726\u2726\u2727\u2727")]
 		E3 = 3,
-		[StringValue("SS<sub>II</sub>")]
+		[StringValue("\u2726\u2726\u2726\u2726\u2727")]
 		E4 = 4,
-		[StringValue("SSS")]
+		[StringValue("\u2726\u2726\u2726\u2726\u2726")]
 		E5 = 5
 	}
 }
